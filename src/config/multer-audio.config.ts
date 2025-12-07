41c649995c35de50d6448bc29de053a9b3910e51
Kartik <kartikya27@gmail.com>
2025-12-07 21:09:36 +0530

    config

--- src/config/multer-audio.config.ts
+++ src/config/multer-audio.config.ts
@@ -1,10 +1,9 @@
-// src/config/multer-audio.config.ts
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 
 export const audioMulterConfig = {
   storage: diskStorage({
-    destination: './uploads/audio', // folder relative to project root
+    destination: './uploads/audio',
     filename: (req, file, cb) => {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
       const ext = extname(file.originalname);
@@ -13,3 +12,37 @@ export const audioMulterConfig = {
     },
   }),
 };
+
+
+
+
+// import { diskStorage } from 'multer';
+// import { extname } from 'path';
+// import type { MulterOptions } from '@nestjs/platform-express';
+
+// export const audioMulterConfig: MulterOptions = {
+//   storage: diskStorage({
+//     destination: './uploads/audio', 
+//     filename: (req, file, cb) => {
+//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//       const ext = extname(file.originalname);
+
+//       const baseName = file.originalname
+//         .replace(ext, '')
+//         .replace(/\s+/g, '-');
+
+//       cb(null, `${uniqueSuffix}-${baseName}${ext}`);
+//     },
+//   }),
+
+//   limits: {
+//     fileSize: 20 * 1024 * 1024, 
+//   },
+
+//   fileFilter: (req, file, cb) => {
+//     if (!file.mimetype.startsWith('audio/')) {
+//       return cb(new Error('Only audio files are allowed'), false);
+//     }
+//     cb(null, true);
+//   },
+// };
