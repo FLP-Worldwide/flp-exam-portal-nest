fe83a4f18288557b0ea26719710389629a32d999
Kartik Mandothiya <55804679+kartikya27@users.noreply.github.com>
2026-01-22 13:12:00 +0530

    try

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,50 @@
-import * as dotenv from 'dotenv';
+// import * as dotenv from 'dotenv';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+// import { join } from 'path';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+//   app.enableCors();
+//   app.useGlobalPipes(new ValidationPipe())
+//   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+//     prefix: '/uploads/',
+//   });
+
+//   await app.listen(process.env.PORT ?? 2000);
+// }
+// bootstrap();
+
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 import { join } from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+const server = express();
+
+async function createApp() {
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(new ValidationPipe());
+
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
 
-  await app.init(process.env.PORT ?? 2000);
+  await app.init(); // ⚠️ listen nahi, init
+  return server;
 }
-bootstrap();
+
+export default createApp;
